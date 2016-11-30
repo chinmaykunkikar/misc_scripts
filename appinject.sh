1ee6dcf16259e79bbb3fafcaebebe497826c3f12
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2016-11-30 18:10:56 +0300

    Rewrite appinject script
    What this script does is that it installs backed-up apps from custom /sdcard location as user apps.
    This will be especially useful after clean ROM installs (i always clean install ROMs almost at a weekly frequency).
    I want to make it a flashable zip to reduce efforts even further. So now I have to write an updater-script required for the flashable zip.
    
    Also, I want to try putting this script directly in the updater binary so that there's no need of even the updater script and additional files in the zip.
    
    P.S. This script is not yet tested, may need changes (to the pm command mostly).

--- appinject.sh
+++ appinject.sh
@@ -1,48 +1,32 @@
+#!/system/bin/sh
+
 # declarations
 SCR="/sdcard/AndroidAssistant_appbackup"
-DEST="/data/app/"
 
 # source apks
-SADM="$SCR/ADM*.apk"
-SCRDEV="$SCR/'Chrome Dev'*.apk"
-SCOC="$SCR/'Clash of Clans'*.apk"
-SCLRO="$SCR/'Clash Royale'*.apk"
-SIG="$SCR/Instagram*.apk"
-SMX="$SCR/MX*.apk"
-SQP="$SCR/QuickPic*.apk"
-SSIT="$SCR/SHAREit*.apk"
-SSC="$SCR/Snapchat*.apk"
-STW="$SCR/Twitter*.apk"
-SWA="$SCR/WhatsApp*.apk"
-SXDA="$SCR/XDA*.apk"
+ADM="$SCR/ADM*.apk"
+CRDEV="$SCR/'Chrome Dev'*.apk"
+COC="$SCR/'Clash of Clans'*.apk"
+CLRO="$SCR/'Clash Royale'*.apk"
+IG="$SCR/Instagram*.apk"
+MX="$SCR/MX*.apk"
+QP="$SCR/QuickPic*.apk"
+SIT="$SCR/SHAREit*.apk"
+SC="$SCR/Snapchat*.apk"
+TW="$SCR/Twitter*.apk"
+WA="$SCR/WhatsApp*.apk"
+XDA="$SCR/XDA*.apk"
 
-# destination folders
-DADM="$DEST/com.dv.adm-1/"
-DCRDEV="$DEST/com.chrome.dev-1/"
-DCOC="$DEST/com.supercell.clashofclans-1/"
-DCLRO="$DEST/com.supercell.clashroyale-1/"
-DIG="$DEST/com.instagram.android-1/"
-DMX="$DEST/com.mxtech.videoplayer.ad-1/"
-DQP="$DEST/com.alensw.PicFolder-1/"
-DSIT="$DEST/com.lenevo.anyshare.gps-1/"
-DSC="$DEST/com.snapchat.android-1/"
-DTW="$DEST/com.twitter.android-1/"
-DWA="$DEST/com.whatsapp-1/"
-DXDA="$DEST/com.xda.labs.play-1/"
-
-# make directories in destination folder
-# partially hardcoding temporarily
-mkdir -p $DADM
-mkdir -p $DCRDEV
-mkdir -p $DCOC
-mkdir -p $DCLRO
-mkdir -p $DIG
-mkdir -p $DMX
-mkdir -p $DQP
-mkdir -p $DSIT
-mkdir -p $DSC
-mkdir -p $DTW
-mkdir -p $DWA
-mkdir -p $DXDA
-
-#continue here
+# install apks via pm
+pm install $ADM
+pm install $CRDEV
+pm install $COC
+pm install $CLRO
+pm install $IG
+pm install $MX
+pm install $QP
+pm install $SIT
+pm install $SC
+pm install $TW
+pm install $WA
+pm install $XDA
